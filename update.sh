5103e5de0c67eddd82441707aef8a017c037eead
yu01 <yuichiyoshida0218@gmail.com>
2015-06-20 18:03:21 +0900

    update app.

---
+++ update.sh
@@ -0,0 +1,4 @@
+
+
+
+curl -X POST -H "X-ChatWorkToken:6ba6e747289567740e689a8fd8d30888" -d "body=Kawaii+がアップデートされました。http://kawaii-inc:morip@kawaii-ios.herokuapp.com/ こちらよりインストールをお願いします。" "https://api.chatwork.com/v1/rooms/21328147/messages"
