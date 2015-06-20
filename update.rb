78e7102422b8250f84290e59344744d2a827c9e0
yu01 <yuichiyoshida0218@gmail.com>
2015-06-20 18:08:39 +0900

    update app.

--- update.rb
+++ update.rb
@@ -1,7 +1,7 @@
 
 %x( git add . )
 %x( git commit -a -m "update app." )
-%x( git push origin master) 
+#{}%x( git push origin master) 
 if %x( git push origin master) 
   %x(curl -X POST -H "X-ChatWorkToken:6ba6e747289567740e689a8fd8d30888" -d "body=Kawaii+がアップデートされました。http://kawaii-inc:morip@kawaii-ios.herokuapp.com/ こちらよりインストールをお願いします。" "https://api.chatwork.com/v1/rooms/21328147/messages")
 end
